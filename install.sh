751991b3fc0be0d61b5f20ca317ca363ceb6c678
Roundstg04 <101461740+Roundstg04@users.noreply.github.com>
2022-03-15 11:27:18 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+read numb
+if [ $numb = "8260541" ]
+then
+    pkg install python -y
+    pkg install dos2unix
+	cp ~/spamMail/mailSPAM.py $PREFIX/bin/mail
+	dos2unix $PREFIX/bin/mail
+	chmod -R 777 ~/mail
+	chmod 777 $PREFIX/bin/mail
+	mail
